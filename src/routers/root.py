d1f1e3281ffe8ee4deef33c9844988d7afdb088c
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-04-09 16:56:43 +0700

    add status code to response

--- src/routers/root.py
+++ src/routers/root.py
@@ -1,4 +1,5 @@
 from docker import DockerClient
+from docker.models.containers import Container
 from fastapi import APIRouter, Depends
 
 from .. import schemas
@@ -13,26 +14,28 @@ def root(
     docker_client: DockerClient = Depends(get_docker_client),
 ) -> dict[str, int | str]:
     lang_to_image = {
-        "js": "node",
+        "js": "node:18-alpine",
     }
 
     image = lang_to_image[code_exec_request.lang]
 
-    if image not in docker_client.images.list():
-        docker_client.images.pull(image)
-
-    run_result = docker_client.containers.run(
-        image,
-        command=["node", "-e", code_exec_request.code],
-        stderr=True,
-        remove=True,
-        mem_limit="8m",
-        nano_cpus=100_000_000,  # 0.1 cores
-        network_disabled=True,
-        privileged=False,
+    container: Container = Container(
+        docker_client.containers.run(
+            image,
+            detach=True,
+            command=["node", "-e", code_exec_request.code],
+            remove=True,
+            mem_limit="8m",
+            nano_cpus=100_000_000,  # 0.1 cores
+            network_disabled=True,
+            privileged=False,
+        )
     )
 
+    run_result = container.wait()
+    output = str(container.logs(stdout=True, stderr=True))
+
     return {
-        "status": 0,
-        "output": str(run_result),
+        "status": int(run_result["StatusCode"]),
+        "output": output,
     }
